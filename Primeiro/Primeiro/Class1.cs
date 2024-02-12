eac91dec9665b8892359e414263993e4f1f05d59
Gabriel de Oliveira Sona <117800662+GBSONA@users.noreply.github.com>
2024-02-12 13:20:24 -0300

    Add files via upload

---
+++ Primeiro/Primeiro/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Primeiro
+{
+    internal class Pessoa
+    {
+        private string _titular;
+        public int Conta { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Pessoa(string nome, int conta) {
+            _titular = nome;
+            Conta = conta;
+        }
+        public Pessoa(string nome, int conta, double saldoInicial) :this(nome, conta)
+        {
+            Deposito(saldoInicial);
+        }
+
+        public string Nome { 
+            get { return _titular; }
+            set {
+                if (value != null && value.Length > 1 )
+                {
+                    _titular = value;
+                }
+            }
+        }
+
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            Saldo = Saldo - 5 - saque;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Conta + ", Titular: " + _titular + ", Saldo: R$:" + Saldo.ToString("F2");
+        }
+
+    }
+}
---
+++ Primeiro/Primeiro/Program.cs
@@ -0,0 +1,52 @@
+﻿using Primeiro;
+using System;
+using System.Globalization;
+
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            
+            Pessoa pessoa;
+
+            Console.Write("Entre o número da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (S/N) ?");
+            string inicial = Console.ReadLine().ToLower();
+            if(inicial == "s")
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double saldo = double.Parse(Console.ReadLine(), CI);
+                pessoa = new Pessoa(titular, conta, saldo);
+            }
+            else
+            {
+                pessoa = new Pessoa(titular, conta);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(pessoa);
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CI);
+            pessoa.Deposito(deposito);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(pessoa);
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CI);
+            pessoa.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(pessoa);
+
+        }
+    }
+}
+
